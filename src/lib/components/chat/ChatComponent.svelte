f0c932945448213a84f432d677be9e51042acac8
2tix <lukasmichalik92@gmail.com>
2023-05-22 13:28:38 +0200

    Finished the client

--- src/lib/components/chat/ChatComponent.svelte
+++ src/lib/components/chat/ChatComponent.svelte
@@ -1,32 +1,54 @@
 <script>
     import "./ChatComponent.scss";
+    // we import the scss file here so that it is bundled with the component
+    import {onMount} from "svelte";
+    // we import the onMount function from svelte, which is called when the component is mounted
+    import {messagesStore} from "../../utils/store.js";
+    // we will use the messagesStore to get the messages when they arrive
+    import {connect, sendMessage} from "../../utils/server.js";
+    // we import the connect and sendMessage functions from the server.js file
 
-    let messages = [{
-        text: "Hello",
-        time: "12:00"
-    },
-        {
-            text: "Hi",
-            time: "12:01"
-        },
-        {
-            text: "How are you?",
-            time: "12:02"
-        }];
+    let messages = [];
+
+    onMount(() => {
+        // we call the onMount function, which is called when the component is mounted
+        connect();
+        // we call the connect function, which connects to the server
+        messagesStore.subscribe(value => {
+            // we subscribe to the messagesStore, which is updated when a new message arrives
+            messages = value;
+            // we set the messages variable to the new value
+        });
+    });
 </script>
 <div class="chat-component">
     <div class="chat">
         <div class="messages">
             {#each messages as message}
-                <div class="message">
+                <!-- We go through every message in the storage -->
+                <div class="message {message.author === 'you' ? 'right' : ''}">
+                    <!-- We add the class "right" to the message if it is from the user -->
                     <div class="message-text">{message.text}</div>
-                    <div class="message-time">{message.time}</div>
                 </div>
+                <div class="message-meta {message.author === 'you' ? 'right' : ''}">{(message.author === "you" ? message.time : " - " + message.author + ` (${message.time})`)}</div>
+                <!-- We add the class "right" to the message meta if it is from the user, we also don't show the author of the message if its our current user -->
             {/each}
         </div>
     </div>
     <div class="input">
-        <input placeholder="Type your message here..." type="text"/>
-        <button>Send</button>
+        <input placeholder="Type your message here..." on:keypress={e => {
+            if(e.key === "Enter" && e.target.value !== "" && e.target.value.trim() !== "") {
+                // we check if the user pressed enter and if the input is not empty
+                sendMessage(e.target.value);
+                e.target.value = "";
+            }
+        }} type="text"/>
+        <button><img width="20px" src="arrow.svg" on:click={e => {
+            if( e.target.value !== "" && e.target.value.trim() !== "") {
+                // we check if the input is not empty
+               sendMessage(e.target.parentNode.parentNode.firstChild.value);
+               e.target.parentNode.parentNode.firstChild.value = "";
+            }
+        }} alt="Send"/></button>
     </div>
 </div>
---
+++ src/lib/utils/server.js
@@ -0,0 +1,36 @@
+import {io} from "socket.io-client";
+import {messagesStore} from "./store.js";
+// we import the socket.io client
+
+let socket;
+let you = "";
+// we will use this for all our communication with the server
+export const connect = () => {
+    socket = io("ws://localhost:3000/");
+    // we connect to the server
+    socket.on("you", res => {
+        you = res;
+    });
+    // we get the name of the user from the server
+    socket.on("messages", res => {
+        messagesStore.set(res.map(message => {
+            if(message.author === you) {
+                return {
+                    ...message,
+                    author: "you"
+                };
+            } else {
+                return message;
+            }
+        }));
+    });
+};
+// this function will be called when the app starts
+
+export const sendMessage = message => {
+    socket.emit("sendMessage", {
+        text: message,
+        time: new Date().toLocaleTimeString([], { hour: '2-digit', minute: '2-digit' })
+    });
+};
+// this function will be called when the user sends a message
